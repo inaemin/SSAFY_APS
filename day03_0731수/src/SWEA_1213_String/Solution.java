f8207d220a20ae6df98807d82d12a7f963408a0a
inaemin <inaemin@gmail.com>
2024-08-01 10:37:45 +0900

    swea 1213 commit

--- day03_0731수/src/SWEA_1213_String/Solution.java
+++ day03_0731수/src/SWEA_1213_String/Solution.java
@@ -1,5 +1,27 @@
 package SWEA_1213_String;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int t=1; t<=10; t++) {
+			int num = sc.nextInt();
+			int cnt = 0;
+			String str = sc.next();
+			String sentence = sc.next();
+			out: for (int i=0; i<sentence.length(); i++) {
+				if (sentence.charAt(i) == str.charAt(0)) {
+					for (int j=1; j<str.length(); j++) {
+						if (i+j > sentence.length()-1 || sentence.charAt(i+j) != str.charAt(j)) {
+							continue out;
+						}
+					}
+					cnt++;
+				}
+			}
+			System.out.println("#"+num+" "+cnt);
+		}
+		sc.close();
+	}
 }
