3a5b4278bb327d8b9517992fc713f196ea82f6e2
inaemin <inaemin@gmail.com>
2024-08-05 01:17:18 +0900

    240804 swea commit

--- day04_0801목/src/SWEA_1208_flatten/Solution.java
+++ day04_0801목/src/SWEA_1208_flatten/Solution.java
@@ -1,5 +1,59 @@
 package SWEA_1208_flatten;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int t=1; t<=10; t++) {
+			int dump = sc.nextInt(); // 덤프 횟수
+			int[] boxHeights = new int[100]; // 박스 높이
+			for (int i=0; i<100; i++) {
+				int ele = sc.nextInt();
+				boxHeights[i] = ele;
+			}
+			
+			int[] count = new int[101]; // 상자높이카운트
+			for (int height : boxHeights) {
+				count[height]++;
+			}
+			
+			int minHeight = 0;
+			int maxHeight = 100;
+			for (int i=0; i<dump; i++) {
+				// 가장 높은 상자 찾기
+				while (count[maxHeight] == 0) {
+					maxHeight--;
+				}
+				// 가장 낮은 상자 찾기
+				while (count[minHeight] == 0) {
+					minHeight++;
+				}
+				
+				// 높이 차이가 1 이하면 더 이상 덤프가 필요 없음
+				if (maxHeight - minHeight <= 1) {
+					break;
+				}
+				
+				// 덤프 수행
+				count[maxHeight]--;
+				count[maxHeight - 1]++;
+				count[minHeight]--;
+				count[minHeight + 1]++;
+			}
+			
+			// 최종 높이 차이 계산
+			while (count[maxHeight] == 0) {
+				maxHeight--;
+			}
+			while (count[minHeight] == 0) {
+				minHeight++;
+			}
+			
+			int result = maxHeight - minHeight;			
+			System.out.println("#"+t+" "+result);
+		}
+		sc.close();
+	}
 }
--- day04_0801목/src/SWEA_1209_Sum/Solution.java
+++ day04_0801목/src/SWEA_1209_Sum/Solution.java
@@ -1,7 +1,46 @@
 package SWEA_1209_Sum;
 
+import java.util.Scanner;
+
 public class Solution {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		for (int t=1; t<= 10; t++) {
+			int test_case = sc.nextInt();
+			int max = 0;
+			int[][] arr = new int[100][100];
+			
+			// 입력받기, 행의 합 계산
+			for (int i=0; i<100; i++) {
+				int row_sum = 0;
+				for (int j=0; j<100; j++) {
+					int ele = sc.nextInt();
+					arr[i][j] = ele;
+					row_sum += ele;
+				}
+				max = Math.max(row_sum, max);
+			}
+			// 열의 합 계산
+			for (int j=0; j<100; j++) {
+				int col_sum = 0;
+				for (int i=0; i<100; i++) {
+					col_sum += arr[i][j];
+				}
+				max = Math.max(col_sum, max);
+			}
+			// 대각선 합 계산
+			int left_diagonal = 0;
+			int right_diagonal = 0;
+			for (int i=0; i<100; i++) {
+				left_diagonal += arr[i][99-i];
+				right_diagonal += arr[i][i];
+			}
+			max = Math.max(left_diagonal, max);
+			max = Math.max(right_diagonal, max);
+			
+			
+			System.out.println("#"+test_case+" "+max);
+		}
+		sc.close();
 	}
 }
