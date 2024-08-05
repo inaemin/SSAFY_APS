614394a9f2aa7ef3e24c23352644870618ebff7c
inaemin <inaemin@gmail.com>
2024-08-05 17:31:04 +0900

    swea 8931 refactoring

--- day05_0805월/src/SWEA_8931_제로/Solution.java
+++ day05_0805월/src/SWEA_8931_제로/Solution.java
@@ -7,22 +7,22 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int tc = sc.nextInt();
+		int tc = sc.nextInt(); // 테스트 케이스 개수
 		for (int t=1; t<=tc; t++) {
-			int k = sc.nextInt();
+			int k = sc.nextInt(); // 입력 개수
 			List<Integer> arr = new ArrayList<>();
+			int sum = 0;
 			for (int i=0; i<k; i++) {
 				int ele = sc.nextInt();
 				if (ele != 0) {
+					sum += ele;
 					arr.add(ele);					
 				} else if (arr.size() > 0) {
+					sum -= arr.get(arr.size()-1);
 					arr.remove(arr.size() - 1);
 				}	
 			}
-			int sum = 0;
-			for (int num : arr) {
-				sum += num;
-			}
+			
 			System.out.println("#"+t+" "+sum);
 		}
 		sc.close();
