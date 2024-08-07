9ccf478117a46254bec0d9f3e0948730ff9349f3
inaemin <inaemin@gmail.com>
2024-08-07 12:17:57 +0900

    swea 7102 commit

--- day07_0807수/src/SWEA_7102_준홍이의카드놀이/Solution.java
+++ day07_0807수/src/SWEA_7102_준홍이의카드놀이/Solution.java
@@ -1,5 +1,27 @@
 package SWEA_7102_준홍이의카드놀이;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for (int t=1; t<=tc; t++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			// n<=m 가정
+			// n으로 만들 수 있는 최소값 => n+1
+			// 1로 만들 수 있는 최대값 사이의 숫자들 => 1+m
+			int min = Math.min(n+1, m+1);
+			int max = Math.max(n+1, m+1);
+			
+			// 출력
+			System.out.print("#"+t);
+			for (int i=min; i<=max; i++) {
+				System.out.print(" "+i);
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
 }
