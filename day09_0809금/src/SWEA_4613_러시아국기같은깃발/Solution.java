d73d8b9802462174c0181d3cc00edd50286752c9
inaemin <inaemin@gmail.com>
2024-08-08 23:23:52 +0900

    swea 6190 commit

---
+++ day09_0809금/src/SWEA_4613_러시아국기같은깃발/Solution.java
@@ -0,0 +1,7 @@
+package SWEA_4613_러시아국기같은깃발;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ day09_0809금/src/SWEA_6190_정곤이의단조증가하는수/Solution.java
@@ -0,0 +1,37 @@
+package SWEA_6190_정곤이의단조증가하는수;
+
+import java.util.Scanner;
+
+public class Solution {
+	private static boolean isMonotonicallyIncreasing(int num) {
+		String numStr = Integer.toString(num);
+		for (int i=0; i<numStr.length()-1; i++) {
+			if (numStr.charAt(i) > numStr.charAt(i+1))
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for (int t=1; t<=tc; t++) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for (int i=0; i<n; i++) {
+				arr[i] = sc.nextInt();
+			}
+			int max = -1;
+			for (int i=0; i<n; i++) {
+				for (int j=i+1; j<n; j++) {
+					int multiple = arr[i] * arr[j];
+					if (isMonotonicallyIncreasing(multiple)) {
+						max = Math.max(max, multiple);
+					}
+				}
+			}
+			System.out.println("#"+t+" "+max);
+		}
+		sc.close();
+	}
+}
