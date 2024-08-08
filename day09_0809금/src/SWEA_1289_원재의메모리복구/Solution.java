c88f43d2733ba40743ade41120f319b317a40310
inaemin <inaemin@gmail.com>
2024-08-08 23:30:40 +0900

    swea 1289 commit

--- day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
+++ day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
@@ -3,34 +3,18 @@ package SWEA_1289_원재의메모리복구;
 import java.util.Scanner;
 
 public class Solution {
-	private static boolean isMonotonicallyIncreasing(int num) {
-		String numStr = Integer.toString(num);
-		for (int i=0; i<numStr.length()-1; i++) {
-			if (numStr.charAt(i) > numStr.charAt(i+1))
-				return false;
-		}
-		return true;
-	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int tc = sc.nextInt();
 		for (int t=1; t<=tc; t++) {
-			int n = sc.nextInt();
-			int[] arr = new int[n];
-			for (int i=0; i<n; i++) {
-				arr[i] = sc.nextInt();
-			}
-			int max = -1;
-			for (int i=0; i<n; i++) {
-				for (int j=i+1; j<n; j++) {
-					int multiple = arr[i] * arr[j];
-					if (isMonotonicallyIncreasing(multiple)) {
-						max = Math.max(max, multiple);
-					}
+			String memory = "0" + sc.next();
+			int cnt = 0;
+			for (int i=0; i<memory.length()-1; i++) {
+				if (memory.charAt(i) != memory.charAt(i+1)) {
+					cnt++;
 				}
 			}
-			System.out.println("#"+t+" "+max);
+			System.out.println("#"+t+" "+cnt);
 		}
 		sc.close();
 	}
