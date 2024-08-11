86f773526de0100ab052bb6d5224ce0ca90b77f0
inaemin <inaemin@gmail.com>
2024-08-11 20:55:09 +0900

    swea 4014 commit

--- day09_0809금/src/swea4014/Solution.java
+++ day09_0809금/src/swea4014/Solution.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// 테스트 케이스 개수
 		int tc = sc.nextInt();
 		for (int t=1; t<=tc; t++) {
+			// 한 변의 길이
 			int n = sc.nextInt();
+			// 경사로의 길이
 			int x = sc.nextInt();
+			// 땅의 높이 입력
 			int[][] land = new int[n][n];
 			for (int i=0; i<n; i++) {
 				for (int j=0; j<n; j++) {
@@ -16,7 +20,90 @@ public class Solution {
 				}
 			}
 			
-			
+			// 활주로가 건설되는 조건
+			// 높이가 변화되는 구간에 설치되어야 하고,
+			// 최소 x개의 동일한 높이의 땅이 있어야 함.
+			// 경사로는 겹치면 안됨.
+			// 행 검사
+			int cnt = 0;
+			for (int i=0; i<n; i++) {
+				// 경사로를 만들 수 있는지의 여부.
+				boolean isPossible = true;
+				// 마지막 경사로가 지어진 인덱스.
+				int lastRunway = -1;
+				out: for (int j=0; j<n-1; j++) {
+					int diff = Math.abs(land[i][j] - land[i][j+1]);
+					if (diff == 1) {
+						// 왼쪽이 낮은 경우
+						if (land[i][j] < land[i][j+1]) {
+							for (int xx=0; xx<x; xx++) {
+								if (j-xx <= lastRunway || j-xx < 0 || land[i][j-xx] != land[i][j]) {
+									isPossible = false;
+									break out;
+								}
+							}
+							lastRunway = j;
+						// 오른쪽이 낮은 경우
+						} else {
+							for (int xx=0; xx<x; xx++) {
+								if (j+1+xx <= lastRunway || j+1+xx >= n || land[i][j+1+xx] != land[i][j+1]) {
+									isPossible = false;
+									break out;
+								}
+							}
+							lastRunway = j+x;
+						}
+						
+					} else if (diff > 1) {
+						isPossible = false;
+						break;
+					} else if (diff == 0) {
+						continue;
+					}
+				}
+				if (isPossible)
+					cnt++;
+			}
+			// 열 검사
+			for (int j=0; j<n; j++) {
+				// 경사로를 지을 수 있는지의 여부.
+				boolean isPossible = true;
+				// 마지막 경사로가 지어진 인덱스.
+				int lastRunway = -1;
+				out : for (int i=0; i<n-1; i++) {
+					int diff = Math.abs(land[i][j] - land[i+1][j]);
+					if (diff == 1) {
+						// 위쪽이 낮은 경우
+						if (land[i][j] < land[i+1][j]) {
+							for (int xx=0; xx<x; xx++) {
+								if (i-xx <= lastRunway || i-xx < 0 || land[i-xx][j] != land[i][j]) {
+									isPossible = false;
+									break out;
+								}
+							}
+							lastRunway = i;
+						// 아랫쪽이 낮은 경우
+						} else {
+							for (int xx=0; xx<x; xx++) {
+								if (i+1+xx <= lastRunway || i+1+xx >= n || land[i+1+xx][j] != land[i+1][j]) {
+									isPossible = false;
+									break out;
+								}
+							}
+							lastRunway = i+x;
+						}
+					} else if (diff > 1) {
+						isPossible = false;
+						break;
+					} else if (diff == 0) {
+						continue;
+					}
+				}
+				if (isPossible)
+					cnt++;
+			}
+			// 출력
+			System.out.println("#"+t+" "+cnt);
 		}
 		sc.close();
 	}
--- day09_0809금/src/swea4615/Solution.java
+++ day09_0809금/src/swea4615/Solution.java
@@ -3,6 +3,42 @@ package swea4615;
 import java.util.Scanner;
 
 public class Solution {
+	private static final char BLACK = 'B';
+	private static final char WHITE = 'W';
+	private static final char EMPTY = '-';
+	
+	private static boolean findMatchingColor(char[][] board, int r, int c, int rowMove, int colMove, char color) {
+		int n = board.length;
+		int pairRow = r;
+		int pairCol = c;
+		System.out.println(rowMove+", "+colMove+", ");
+		while (true) {
+			pairRow += rowMove;
+			pairCol += colMove;
+			if (pairRow >= 0 && pairRow < n && pairCol >= 0 && pairCol < n) {
+				if (board[pairRow][pairCol] == color) {
+					flipStoneBetween(board, r, c, pairRow, pairCol, rowMove, colMove, color);
+					return true;
+				} else if (board[pairRow][pairCol] == EMPTY) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+	
+	private static void flipStoneBetween(char[][] board, int startRow, int startCol, int endRow, int endCol, int rowMove, int colMove, char color) {
+		int r = startRow;
+		int c = startCol;
+		while (r != endRow || c != endCol) {
+			if (board[r][c] != color) 
+				board[r][c] = color;
+			r += rowMove;
+			c += colMove;
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		// 테스트 케이스 개수
@@ -13,100 +49,56 @@ public class Solution {
 			// 1이면 흑돌, 2이면 백돌
 			// (2, 3) -> 배열상에는 (2, 1) 좌표로 변경
 			char [][] board = new char[n][n];
+			// EMPTY로 초기화
+			for (int i=0; i<n; i++) {
+				for (int j=0; j<n; j++) {
+					board[i][j] = EMPTY;
+				}
+			}
 			// 가운데 4개 배치
-			board[n/2-1][n/2-1] = 'W';
-			board[n/2][n/2] = 'W';
-			board[n/2][n/2-1] = 'B';
-			board[n/2-1][n/2] = 'B';
+			board[n/2-1][n/2-1] = WHITE;
+			board[n/2][n/2] = WHITE;
+			board[n/2][n/2-1] = BLACK;
+			board[n/2-1][n/2] = BLACK;
 			// 흑돌, 백돌 놓기
 			for (int p=0; p<m; p++) {
 				int y = sc.nextInt() - 1;
 				int x = sc.nextInt() - 1;
-				char color = sc.nextInt() == 1 ? 'B' : 'W';
+				char color = sc.nextInt() == 1 ? BLACK : WHITE;
 				board[x][y] = color;
 				// 수직검사
-				boolean hasSameColor = false;
-				for (int i=0; i<=x-1; i++) {
-					if (board[i][y] == color)
-						hasSameColor = true;						
-					if (hasSameColor == true && board[i][y] != color)
-						board[i][y] = color;
-				}
-				hasSameColor = false;
-				for (int i=n-1; i>=x+1; i--) {
-					if (board[i][y] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[i][y] != color)
-						board[i][y] = color;
-				}
-				hasSameColor = false;
+				findMatchingColor(board, x, y, -1, 0, color);
+				findMatchingColor(board, x, y, 1, 0, color);
 				// 수평검사
-				for (int j=0; j<=y-1; j++) {
-					if (board[x][j] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[x][j] != color)
-						board[x][j] = color;
-				}
-				hasSameColor = false;
-				for (int j=n-1; j>=y+1; j--) {
-					if (board[x][j] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[x][j] != color)
-						board[x][j] = color;
-				}
-				hasSameColor = false;
+				findMatchingColor(board, x, y, 0, -1, color);
+				findMatchingColor(board, x, y, 0, 1, color);
 				// 왼쪽대각선검사
 				// x,y 둘 중에 작은
 				// (3,2)라면 (2,1) (1,0)
 				//          (4,3)
-				for (int i=Math.min(x, y); i>=1; i--) {
-					if (board[x-i][y-i] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[x-i][y-i] != color)
-						board[x-i][y-i] = color;
-				}
-				hasSameColor = false;
-				for (int i=n-1-Math.max(x, y); i>=1; i--) {
-					if (board[x+i][y+i] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[x+i][y+i] != color)
-						board[x+i][y+i] = color;
-				}
-				hasSameColor = false;
+				findMatchingColor(board, x, y, -1, -1, color);
+				findMatchingColor(board, x, y, 1, 1, color);
 				// 오른쪽대각선검사
 				// (3, 2)라면 (2, 3), (1, 4) (0, 5)
 				// (4, 1), (5, 0)
 				// x는 빼고 y는 더함.
-				for (int i=Math.min(x, n-1-y); i>=1; i--) {
-					if (board[x-i][y+i] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[x-i][y+i] != color)
-						board[x-i][y+i] = color;
-				}
-				hasSameColor = false;
-				for (int i=Math.min(n-1-x, y); i>=1; i--) {
-					if (board[x+i][y-i] == color)
-						hasSameColor = true;
-					if (hasSameColor && board[x+i][y-i] != color)
-						board[x+i][y-i] = color;
-				}
-				hasSameColor = false;
+				findMatchingColor(board, x, y, -1, 1, color);
+				findMatchingColor(board, x, y, 1, -1, color);
 			}
-			
 			// 흑돌, 백돌 개수 세기
 			int b_count = 0;
 			int w_count = 0;
 			for (int i=0; i<n; i++) {
 				for (int j=0; j<n; j++) {
-					if (board[i][j] == 'B')
+					if (board[i][j] == BLACK)
 						b_count++;
-					else if (board[i][j] == 'W')
+					else if (board[i][j] == WHITE)
 						w_count++; 
 				}
 			}
 			// 출력
 			System.out.println("#"+t+" "+b_count+" "+w_count);
 		}
-		sc.close();
+	sc.close();
 	}
 }
